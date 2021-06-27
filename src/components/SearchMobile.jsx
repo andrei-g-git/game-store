8fe68e1c522e5ad28245e19f9dcc7ad70a9cd448
humpty dumpty <spyker5@creationuq.com>
2021-06-27 16:12:46 +0300

    search transitions to the left

--- src/components/SearchMobile.jsx
+++ src/components/SearchMobile.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { /* withRouter, */ useHistory } from 'react-router-dom';
 import * as actions from '../js/redux/actions';
 import searchIcon from '../assets/img/search-white.png';
+import searchIconDark from '../assets/img/search-1.png';
 import '../css/SearchMobile.scss';
 
 import { useEffect } from 'react';
@@ -11,25 +13,21 @@ const $ = require('jquery');
 function SearchMobile(props) {
 
     useEffect(() => {
-        // $('img').on('focusout', () => {
-        //     $(this).removeClass('display-none');
-        // })
         const form = document.getElementsByClassName("mobile-search-form")[0];
         form.addEventListener("focusout", (event) => {
             props.toggleSearch(false);
         });
     }, [])
 
+    let history = useHistory();
+
     let searchIconVisibilityClass;
-    let formVisibilityClass;
     let formTransitionClass;
     if(props.clicked){
         searchIconVisibilityClass = "icon-display-none";
-        //formVisibilityClass = "form-display-block";
         formTransitionClass = " form-transition-expand";
     } else {
         searchIconVisibilityClass = "icon-display-block";
-        //formVisibilityClass = "form-display-none";
         formTransitionClass = " form-transition-contract";
     }
 
@@ -40,28 +38,47 @@ function SearchMobile(props) {
                 alt="search"
                 onClick={() => props.toggleSearch(true)}
             />
-            <form className={"mobile-search-form " /* + formVisibilityClass */ + formTransitionClass}
-                onSubmit={console.log('abc')}
+            <form className={"mobile-search-form " + formTransitionClass}
+                onSubmit={curryHandleSubmit(props, history)}
             >
                 <input className="mobile-search-field"
                     type="text"
                     placeholder="Search..."
+                />
+                <input className="mobile-search-button"
+                    src={searchIconDark}
+                    alt="search"
+                    type="image"
                 />
             </form>
         </div>
     )
 }
 
+const curryHandleSubmit = (props, history) =>{
+    return function(event){
+        event.preventDefault(); 
+        const searchFiled = document.getElementsByClassName("mobile-search-field")[0];
+        const searchQuery = searchFiled.value;
+        props.storeSearch(searchQuery);
+        history.push("/search-result");
+    }
+}
+
 const mapStateToProps = (state) => {
     return{
-        clicked: state.uiReducer.mobileSearchToggled
+        clicked: state.uiReducer.mobileSearchToggled,
+        searchQuery: state.databaseReducer.searchQuery
     }
 }
 const mapDispatchToProps = (dispatch) => {
     return{
         toggleSearch: (visible) => {
             dispatch(actions.mobileSearchToggled(visible));
-        }
+        },
+        storeSearch: (searchQuery) => {
+            dispatch(actions.searchQueried(searchQuery))
+        }        
     }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(SearchMobile);
+export default /* withRouter( */connect(mapStateToProps, mapDispatchToProps)(SearchMobile)/* ) */;
